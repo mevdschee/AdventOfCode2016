92eddf3733bf59c8c323728f16fef79099e860b5
Maurits van der Schee <maurits@vdschee.nl>
2016-12-03 11:44:14 +0100

    Tested a bit.. seems to work now

--- start.sh
+++ start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 if [ $# -lt 1 ]; then
-  echo "Argument DAY missing".
+  echo "Argument DAY missing"
   exit 1
 fi;
 DAY=`printf %02d $1`
 if [ ! -f day$DAY.txt ]; then
   code .
   firefox http://adventofcode.com/2016/day/$1
-  wget http://adventofcode.com/2016/day/$1/input -O day$DAY.txt -o /dev/null
-  touch day${DAY}a.php
+  firefox http://adventofcode.com/2016/day/$1/input
+  touch day${DAY}.txt
+  echo "<?php" > day${DAY}a.php
+  echo "\$inputs = file('day${DAY}.txt');" >> day${DAY}a.php
   touch day${DAY}b.php
 fi;
