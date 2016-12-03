b75fed80e16cc7b01cb4322a35b6f4f756d83d26
Maurits van der Schee <maurits@vdschee.nl>
2016-12-03 11:32:00 +0100

    Prepare for day 10 and up

--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-DAY=$1
+if [ $# -lt 1 ]; then
+  echo "Argument DAY missing".
+  exit 1
+fi;
+DAY=`printf %02d $1`
 if [ ! -f day$DAY.txt ]; then
-  firefox http://adventofcode.com/2016/day/$DAY
-  wget http://adventofcode.com/2016/day/$DAY/input -O day$DAY.txt -o /dev/null
+  code .
+  firefox http://adventofcode.com/2016/day/$1
+  wget http://adventofcode.com/2016/day/$1/input -O day$DAY.txt -o /dev/null
   touch day${DAY}a.php
   touch day${DAY}b.php
 fi;
