1916bf44e6bd31902dcb1ee2da93140eff75a6ca
Maurits van der Schee <maurits@vdschee.nl>
2016-12-06 07:56:16 +0100

    Day 06.. not too hard

---
+++ day06b.php
@@ -0,0 +1,17 @@
+<?php
+$inputs = file('day06.txt');
+$columns = array();
+for ($i=0;$i<strlen(trim($inputs[0]));$i++) {
+  $columns[$i]=array();
+  foreach ($inputs as $input) {
+    $columns[$i][]=$input[$i];
+  }
+}
+$pwd = '';
+foreach ($columns as $column) {
+  $counts = array_count_values($column);
+  asort($counts);
+  $keys = array_keys($counts);
+  $pwd .= $keys[0];
+}
+var_dump($pwd);
