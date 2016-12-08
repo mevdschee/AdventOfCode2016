bcae42eddfc9be04eadf0f507e4c3f6fddd8300d
Maurits van der Schee <maurits@vdschee.nl>
2016-12-08 19:00:01 +0100

    Day 07

---
+++ day07a.php
@@ -0,0 +1,20 @@
+<?php
+$inputs = file('day07.txt');
+$sum = 0;
+foreach ($inputs as $input){
+  $strs = array('','');
+  $input = str_replace('[',']',trim($input));
+  foreach (explode(']',$input) as $i=>$str) {
+    $strs[$i%2].=' '.$str;
+  }
+  $abba = function($s) {  
+    if (preg_match('/([a-z])([a-z])\2\1/',$s,$matches)) {
+      if ($matches[1]!==$matches[2]) {
+        return true;
+      }
+    }
+    return false;
+  };
+  if ($abba($strs[0]) && !$abba($strs[1])) $sum++;
+}
+var_dump($sum);
