b380f1bb950d680285146a77e5959db93339770a
Maurits van der Schee <maurits@vdschee.nl>
2016-12-09 09:42:37 +0100

    Day 09

---
+++ day09b.php
@@ -0,0 +1,26 @@
+<?php
+$str = trim(file_get_contents('day09.txt'));
+
+function decode($str) {
+  $decoded = '';
+  $current = 0;
+  while (true) {
+    $start = strpos($str,'(',$current);
+    if ($start===false) {
+        $decoded .= substr($str,$current);
+        break;
+    }
+    $decoded .= substr($str,$current,$start-$current);
+    $end = strpos($str,')',$start)+1;
+    $instruction = substr($str,$start,$end-$start);
+    $instruction = explode('x',trim($instruction,'()'));
+    $repeat = substr($str,$end,$instruction[0]);
+    $decoded .= str_repeat($repeat,$instruction[1]);
+    $current = $end+$instruction[0];
+  }
+  $start = strpos($decoded,'(');
+  if ($start===false) return strlen($decoded);  
+  return decode($decoded);
+}
+ 
+var_dump(decode($str));
