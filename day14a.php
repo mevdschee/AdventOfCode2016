edee857ae0133d58abf399e06362df32825d1583
Maurits van der Schee <maurits@vdschee.nl>
2016-12-14 08:35:11 +0100

    Day 14

---
+++ day14a.php
@@ -0,0 +1,15 @@
+<?php
+$salt = trim(file_get_contents('day14.txt'));
+$keys = array();
+for ($i=0;count($keys)<64;$i++) {
+  $hash = md5($salt.$i);
+  if (!preg_match('/([a-z0-9])\1\1/',$hash,$matches)) continue;
+  for ($j=$i+1;$j<$i+1001;$j++) {
+    $hash = md5($salt.$j);
+    if (strpos($hash,str_repeat($matches[1],5))!==false) {
+        if (!in_array($i,$keys)) $keys[]=$i;
+        break;
+    }
+  }
+}
+var_dump($keys[63]);
---
+++ day14b.php
@@ -0,0 +1,26 @@
+<?php
+function cached_md5($salt,$str){
+  static $cache=array();
+  if (isset($cache[$str])) return $cache[$str];
+  $hash = md5($salt.$str);
+  for ($i=0;$i<2016;$i++) {
+    $hash = md5($hash);
+  }
+  $cache[$str] = $hash;
+  return $hash;
+}
+
+$salt = trim(file_get_contents('day14.txt'));
+$keys = array();
+for ($i=0;count($keys)<64;$i++) {
+  $hash = cached_md5($salt,$i);
+  if (!preg_match('/([a-z0-9])\1\1/',$hash,$matches)) continue;
+  for ($j=$i+1;$j<$i+1001;$j++) {
+    $hash = cached_md5($salt,$j);
+    if (strpos($hash,str_repeat($matches[1],5))!==false) {
+        if (!in_array($i,$keys)) $keys[]=$i;
+        break;
+    }
+  }
+}
+var_dump($keys[63]);
